5beb4a0ba60f474ffb014a6bd60da0cfa83f4feb
artdev-hash <kim.dev.pr@gmail.com>
2024-02-22 06:30:12 +0800

    site

---
+++ config.js
@@ -0,0 +1,14 @@
+window.AboutUsConfig = {
+  About: 'MK PRODUCTS',
+  Category: 'Food',
+  Type: 'CHICKEN / PORK PRODUCTS<',
+  contacts: {
+    message: 'FOR INQUIRIES, visit our facebook page',
+    links: [
+      {
+        name: 'Facebook',
+        link: 'https://www.facebook.com/profile.php?id=61556236304617'
+      }
+    ]
+  }
+};
